d920770906dc09d3d5392230ca4a4c633d74b707
gettoknowdavid <knowdavidmichael@gmail.com>
2026-05-20 13:18:44 +0100

    refactor: Update Prometheus config, streamline routes, and clean up Docker Compose
    - Adjusted Prometheus configuration to update scrape targets and add `metrics_path`.
    - Simplified route initialization by consolidating user and auth routes.
    - Removed unused `network: host` config from Docker Compose.

--- apps/api/src/routes/mod.rs
+++ apps/api/src/routes/mod.rs
@@ -9,17 +9,9 @@ pub mod health;
 pub mod user;
 
 pub fn build_meno_routes(state: Arc<MenoState>) -> Router<Arc<MenoState>> {
-    let public_routes = Router::new()
-        .route("/health", get(health::health_handler))
-        .nest("/api/v1/auth", auth::router());
-
-    // let protected_routes = Router::new()
-    //     .nest("/api/v1/users", user::router())
-    //     .layer(from_fn_with_state(state.clone(), auth_middleware))
-    //     .layer(with_rate_limit(25, 60));
-
     Router::new()
-        .merge(public_routes)
+        .route("/health", get(health::health_handler))
+        .nest("/api/v1/auth", auth::router())
         .nest("/api/v1/users", user::router(state.clone()))
         .layer(with_rate_limit(25, 60))
 }
